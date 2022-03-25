0d1f4e36cbad151e3201c80f8c48a8694077bf83
Innula2 <91963982+Innula2@users.noreply.github.com>
2022-03-25 21:28:26 +0300

    Добавьте файлы проекта.

---
+++ AppRelationModel4/Company.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppRelationModel4
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<User> Users { get; set; } = new List<User>();
+        var companies = db.Companies
+ .Include(c => c.Users) // добавляем данные по пользователям
+ .ToList();
+foreach (var company in companies)
+{
+ Console.WriteLine(company.Name);
+ // выводим сотрудников компании
+ foreach (var user in company.Users)
+ Console.WriteLine(user.Name);
+ Console.WriteLine("----------------------"); // для красоты
+
+}
+
+}
+}
---
+++ AppRelationModel4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppRelationModel4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // пересоздадим базу данных
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                // добавляем начальные данные
+                Company microsoft = new Company { Name = "Microsoft" };
+                Company google = new Company { Name = "Google" };
+                db.Companies.AddRange(microsoft, google);
+
+                User tom = new User { Name = "Tom", Company = microsoft };
+                User bob = new User { Name = "Bob", Company = google };
+                User alice = new User { Name = "Alice", Company = microsoft };
+                User kate = new User { Name = "Kate", Company = google };
+                db.Users.AddRange(tom, bob, alice, kate);
+
+                db.SaveChanges();
+                // получаем пользователей
+                var users = db.Users
+                .Include(u => u.Company) // подгружаем данные по компаниям
+                .ToList();
+                foreach (var user in users)
+                    Console.WriteLine($"{user.Name} - {user.Company?.Name}");
+            }
+        }
+    }
+}
